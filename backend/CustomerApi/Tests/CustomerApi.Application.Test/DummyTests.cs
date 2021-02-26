f4e9432797cef2ff117283f658a7b4ff908e9b46
Marko Majamäki <marko.majamaki@gmail.com>
2021-02-26 07:38:31 +0200

    Add test project for CustomerApi.Application and one dummy test

---
+++ backend/CustomerApi/Tests/CustomerApi.Application.Test/DummyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomerApi.Application.Test
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
